42862f5726fdb42b902f605ae36b036bcf476ae7
sanghunlee <simoniful0@gmail.com>
2023-07-18 23:48:40 +0900

    chore: reset

--- BandalArt/Projects/Core/Sources/Core.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Core.swift
-//  ProjectDescriptionHelpers
-//
-//  Created by Sang hun Lee on 2023/07/13.
-//
-
-import Foundation
--- BandalArt/Projects/Feature/Tests/FeatureTests.swift
+++
@@ -1,8 +0,0 @@
-//
-//  FeatureTests.swift
-//  ProjectDescriptionHelpers
-//
-//  Created by Sang hun Lee on 2023/07/13.
-//
-
-import Foundation
