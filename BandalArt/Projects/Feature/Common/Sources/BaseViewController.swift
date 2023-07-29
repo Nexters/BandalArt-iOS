f5a42574d095e4eac8ebbad07800a42d4c610444
Simon <simoniful0@gmail.com>
2023-07-29 12:26:20 +0900

    feat: BaseVC, BaseCell 구성 및 진입점 임시 설정

--- BandalArt/Projects/Feature/Common/Sources/BaseViewController.swift
+++ BandalArt/Projects/Feature/Common/Sources/BaseViewController.swift
@@ -15,7 +15,7 @@ import SnapKit
 open class BaseViewController: UIViewController {
   private let activityIndicator = UIActivityIndicatorView(style: .medium)
   open var cancellables: Set<AnyCancellable> = []
-  
+
   open override func viewDidLoad() {
     super.viewDidLoad()
     setupAttribute()
@@ -23,19 +23,19 @@ open class BaseViewController: UIViewController {
     setupConstraints()
     bind()
   }
-  
+
   public init() {
     super.init(nibName: nil, bundle: nil)
   }
-  
+
   deinit{
     print("\(type(of: self)): \(#function)")
   }
-  
+
   required public init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
-  
+
   open func setupAttribute() {}
   open func setupView() {}
   open func setupConstraints() {
@@ -45,9 +45,9 @@ open class BaseViewController: UIViewController {
       $0.size.equalTo(50)
     }
   }
-  
+
   open func bind() {}
-  
+
   open func startIndicator() {
     activityIndicator.startAnimating()
   }
