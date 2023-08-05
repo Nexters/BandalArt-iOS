4ac042cbf6de4a8a2f76410114b0b3062c827d00
Simon <simoniful0@gmail.com>
2023-08-06 01:13:45 +0900

    chore: 남아있던 모듈 잔류 파일 정리

--- BandalArt/Projects/Core/Database/Sources/Database.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Database.swift
-//  Config
-//
-//  Created by Sang hun Lee on 2023/07/23.
-//
-
-import Foundation
--- BandalArt/Projects/Core/Database/Tests/DatabaseTests.swift
+++
@@ -1,8 +0,0 @@
-//
-//  DatabaseTests.swift
-//  Config
-//
-//  Created by Sang hun Lee on 2023/07/23.
-//
-
-import Foundation
